4bdfbc71a65fbc0acad5315024b14e9e4657e515
charozacchigna <charo.zacchigna@gmail.com>
2022-08-12 19:17:50 -0300

    wip:agregado de argumentos posibles

--- translate.py
+++ translate.py
@@ -3,13 +3,28 @@ import argparse
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="")
-    parser.add_argument("", 
-                        metavar="", 
-                        nargs="+/?", #creo que ? lo hace optativo
-                        choices=[], 
+    parser.add_argument("source", 
+                        metavar="SOURCE", 
+                        nargs="+", #creo que ? lo hace optativo 
                         type= str, 
                         #action= "store_true", #no me acuerdo que hace
                         #default= "", #no es obligatorio
-                        help="")
+                        help="Indicar el nombre del archivo a traducir")
+    parser.add_argument("language", 
+                        metavar="LAN", 
+                        nargs="?", #creo que ? lo hace optativo
+                        type= str, 
+                        #action= "store_true", #no me acuerdo que hace
+                        default= "es", #no es obligatorio
+                        help="Seleccionar el idioma")
+    parser.add_argument("grammar", 
+                        metavar="GRAMMAR", 
+                        nargs="?", #creo que ? lo hace optativo 
+                        type= str, 
+                        #action= "store_true", #no me acuerdo que hace
+                        default= "cfg", #no es obligatorio
+                        help="Seleccionar el tipo de gramàtica")
 
-    args = parser.parse_args()    #desde acà se llama como args.variable
+    args = parser.parse_args()
+
+    translator.translate(args.source, args.language, args.grammar)
