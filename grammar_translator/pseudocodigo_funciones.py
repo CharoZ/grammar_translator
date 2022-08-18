b3db23373516f1de8a47d5ec6661aa12b2696862
charozacchigna <charo.zacchigna@gmail.com>
2022-08-18 18:42:35 -0300

    feat: Subida de esqueleto pseudocodigo

---
+++ grammar_translator/pseudocodigo_funciones.py
@@ -0,0 +1,26 @@
+def funcion_que_levanta_gram_categorial(nombre_archivo):
+ return None
+
+def funcion_que_carga_las_reglas(idioma='es', gram='cfg'):
+    return None
+
+def funcion_que_preprocesa_input(gramatica):
+    #Harìa los split, distingue si es terminal o no?
+    #Devuelve terminales y no terminales preprocesados por separado?
+    #Terminales en formato diccionario: categorìa: [terminal1, terminal2]
+    return None
+
+def funcion_que_traduce_simbolos_terminales(diccionario_terminales):
+    #ver còdigo de archivo de traducciòn
+    return None
+
+def funcion_que_busca_reglas_para_las_categorias(diccionario_no_terminales):
+    return None
+
+def funcion_que_unifica_outputs_de_las_dos_anteriores_y_check_final(trad_terminales, reglas):
+    #Unifica y chequea que todas las categorias de las reglas tengan algùn terminal
+    #caso contrario las saca
+    return None
+
+def funcion_que_guarda_cfg_final(gram_completa):
+    return None
