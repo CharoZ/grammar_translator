48f7d8bb131616c10be05d26768eed9b38c4c5d0
charozacchigna <charo.zacchigna@gmail.com>
2022-08-30 12:16:09 -0300

    WIP: Script con nombre tentativo y WIP de funcion

---
+++ grammar_translator/translator.py
@@ -0,0 +1,18 @@
+def preprocesamiento(gramatica_categorial):
+    '''
+        Funcion de preprocesado de la gramatica categorial. Devuelve
+        simbolos terminales y no terminales con su categoria.
+        Parametros
+        ----------
+        gramtica_categorial: str
+            Gramatica para preprocesar.
+            
+        Returns
+        -------
+
+        
+        #Harìa los split, distingue si es terminal o no?
+        #Devuelve terminales y no terminales preprocesados por separado?
+        #Terminales en formato diccionario: categorìa: [terminal1, terminal2]
+    '''
+    return None
