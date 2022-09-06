7115b31ff75b15528ed015d35492e4a4970892ed
Sofia-Benitez <sofiaaldanabenitez@gmail.com>
2022-09-06 15:23:46 -0300

    feat: modificación en la fn apertura de banco de reglas

--- grammar_translator/trabajo_archivos.py
+++ grammar_translator/trabajo_archivos.py
@@ -3,19 +3,27 @@ import json
 
 #Función que abre el txt con la gramatica categorial. Recibe el nombre del archivo por parametro y devuelve un string con la gramática
 def abrir_gramatica_categorial(nombre_archivo):
-    path="gramaticas/" + nombre_archivo + ".txt"
+    path = f"gramaticas/{nombre_archivo}.txt"
     with open(path,'r', encoding="utf-8") as archivo:
         gramatica = archivo.read()
 
     return gramatica
 
-#Función que abre el diccionario de reglas que corresponda según el idioma y la gramática. Devuelve el diccionario de reglas
+#Función que devuelve el diccionario de reglas que corresponda según el idioma y la gramática.
+import json
+
 def cargar_diccionario_reglas(idioma, gramatica):
-    if(idioma =='es'and gramatica =='cfg'):
-        with open('../data/cfg.json', 'r') as archivo:
-            diccionario = json.load(archivo)
     
-    return diccionario
+    path = f"../data/{gramatica}.json"
+    with open(path, 'r') as file:
+        diccionarios = json.load(file)
+        
+    for key in diccionarios:
+        if(key==idioma):
+            banco_reglas=diccionarios[key]
+            return banco_reglas
+        else:
+            return None
 
 #Funcion que recibe una gramatica en forma de lista, la convierte en un string separado por /n 
 #Guarda la gramática resultante en un archivo (CFG)s
